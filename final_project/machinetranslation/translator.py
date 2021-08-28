476abd543891b2032d04f39a2b8f92348301fd28
AKoltun91 <alex.koltun@zurichservices.com>
2021-08-28 03:36:08 +0000

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,16 +8,17 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
-
+#Instantiate Language Translator
+#Code based off code I learned from Python for Data Science, AI & Development course with Coursera
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(version='2018-05-01',authenticator=authenticator)
+language_translator.set_service_url(url)
 
 def english_to_french(english_text):
     '''Function translates from English to French'''
     if english_text is None:
         return ""
-    #Code based off a previous course I took with Coursera
-    authenticator = IAMAuthenticator(apikey)
-    language_translator = LanguageTranslatorV3(version='2018-05-01',authenticator=authenticator)
-    language_translator.set_service_url(url)
+    #Code based off code I learned from Python for Data Science, AI & Development course
     #Translate response object
     translation_response = language_translator.translate(\
     text=english_text, model_id='en-fr')
@@ -30,10 +31,7 @@ def french_to_english(french_text):
     '''Function translates from French to English'''
     if french_text is None:
         return ""
-    #Code based off a previous course I took with Coursera
-    authenticator = IAMAuthenticator(apikey)
-    language_translator = LanguageTranslatorV3(version='2018-05-01',authenticator=authenticator)
-    language_translator.set_service_url(url)
+    #Code based off code I learned from Python for Data Science, AI & Development course
     #Translate response object
     translation_response = language_translator.translate(\
     text=french_text, model_id='fr-en')
